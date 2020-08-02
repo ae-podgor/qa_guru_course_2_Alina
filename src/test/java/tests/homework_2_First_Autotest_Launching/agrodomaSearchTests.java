9c485823148c6b5c01bf8709ad8cfc746336f9ab
ae-podgor <ae.podgor@gmail.com>
2020-08-02 19:19:08 +0300

    Добавлена проверка поиска несуществующего товара.

--- src/test/java/tests/homework_2_First_Autotest_Launching/agrodomaSearchTests.java
+++ src/test/java/tests/homework_2_First_Autotest_Launching/agrodomaSearchTests.java
@@ -1,8 +1,11 @@
 package tests.homework_2_First_Autotest_Launching;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class agrodomaSearchTests {
@@ -19,8 +22,16 @@ public class agrodomaSearchTests {
     // Нажать на лупу и ввести название растения
     $(".fa-search").click();
     $(getFocusedElement()).setValue(plantName).pressEnter();
-
+    // Проверить, что в результатах есть это растение
+    $(byText("Товары, соответствующие критериям поиска")).shouldHave(text(plantName));
   }
 
-
+  @Test
+  void notExistItemSearch() {
+    // Нажать на лупу и ввести название растения
+    $(".fa-search").click();
+    $(getFocusedElement()).setValue("blah-blah").pressEnter();
+    // Проверить, что в результат поиска ничего нет
+    $(".content").shouldHave(text("Нет товаров, соответствующих критериям поиска."));
+  }
 }
