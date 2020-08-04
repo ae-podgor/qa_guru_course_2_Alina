6085c26f4c92fd47f8fd7ba2c496e8f03c59d315
ae.podgor <ae.podgor@gmail.com>
2020-08-04 21:44:35 +0300

    Написан тест на использование sibling(), preceding(), parent(), closest() для перехода на страхование вкладов

--- src/test/java/tests/homework3_Selenide_1/Alfabank.java
+++ src/test/java/tests/homework3_Selenide_1/Alfabank.java
@@ -1,9 +1,12 @@
 package tests.homework3_Selenide_1;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
+
 import static com.codeborne.selenide.CollectionCondition.*;
 
 public class Alfabank {
@@ -14,6 +17,18 @@ public class Alfabank {
         $(byTitle("Вклады")).hover();
         $(byTitle("Депозиты")).click();
         $(byText("Архивные депозиты")).click();
+        $("h3").shouldHave(text("Архивные депозиты"));
         $$(".product-cell__cell").shouldHave(size(3));
     }
+
+    @Test
+    void insuranceTransit() {
+        open("https://alfabank.ru/make-money/savings-account/");
+
+        $(byTitle("Бесплатные сервисы для накоплений")).closest(".sub-navigation").$(byTitle("Депозиты"))
+                .parent().sibling(4).preceding(0).shouldHave(text("Страхование вкладов")).click();
+
+        $("h1").shouldHave(text("Страхование вкладов"));
+    }
+
 }
